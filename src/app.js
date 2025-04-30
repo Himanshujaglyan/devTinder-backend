74f23493288f5e04da0861966eb6321455cbd66b
Himanshujaglyan <114854175+Himanshujaglyan@users.noreply.github.com>
2025-04-30 16:13:28 +0530

    netlify

--- src/app.js
+++ src/app.js
@@ -61,7 +61,7 @@ const cookieParser = require("cookie-parser")
     app.use(express.json());//this is middleware which helps to convert json into js object because server can't undertand json directly
     app.use(cookieParser());
     app.use(cors({
-        origin:true,
+        origin:"https://devhim1.netlify.app/",
         credentials:true
     }));
     app.get("/", (req, res) => {
