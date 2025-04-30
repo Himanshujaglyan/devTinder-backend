87813e6d6b60d563cdda01fdf6a5e30ef68333db
Himanshujaglyan <114854175+Himanshujaglyan@users.noreply.github.com>
2025-04-30 15:31:14 +0530

    logout api change

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -86,10 +86,19 @@ authrouter.post("/login" ,async(req,res) => {
 
 
 //Logout
-authrouter.post("/logout", catchAsync(async (req,res)=>{
-    res.cookie("token", null ,{
-        expires : new Date(Date.now()),
-    }).send("logout successfully!!")
-}))
-
+// authrouter.post("/logout", catchAsync(async (req,res)=>{
+//     res.cookie("token", null ,{
+//         expires : new Date(Date.now()),
+//     }).send("logout successfully!!")
+// }))
+authrouter.post("/logout", catchAsync(async (req, res) => {
+    res.cookie("token", null, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      expires: new Date(0),
+    });
+    res.send("Logout successfully!");
+  }));
+  
 module.exports = authrouter;
